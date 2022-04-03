a2e6efea53eb3f04dd4ea0dc3a7af02f7852e2fc
Samy <dev.exelight@gmail.com>
2022-04-03 10:41:03 +0100

    Analog Part completed

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Yo");
+let hours = document.querySelector('#hours');
+let minutes = document.querySelector('#minutes');
+let seconds = document.querySelector('#seconds');
+
+setInterval(() => {
+  let day = new Date();
+  let hr = day.getHours() * 30;
+  let min = day.getMinutes() * 6;
+  let sec = day.getSeconds() * 6;
+
+  hours.style.transform = `rotate(${hr+(min/12)}deg)`;
+  minutes.style.transform = `rotate(${min+(sec/60)}deg)`;
+  seconds.style.transform = `rotate(${sec}deg)`;
+})
